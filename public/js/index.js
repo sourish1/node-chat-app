f1d24064252ec12f3b374f968f83fffef669ca66
Sourish1 <sourishkumarmusib@gmail.com>
2018-08-12 01:51:03 +0530

    add geolocation feature

--- public/js/index.js
+++ public/js/index.js
@@ -23,6 +23,16 @@ socket.on('newMessage', function (message) {
 //     console.log('Got it', data);
 // });
 
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from} `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
 
@@ -31,5 +41,23 @@ jQuery('#message-form').on('submit', function(e){
         text: jQuery('[name=message]').val()
     }, function(){
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        //console.log(position);
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function(){
+        alert('Unable to fetch location');
     });
 });
