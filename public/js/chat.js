7151d8f7288b8483644b29adac6a0b3f411d195b
Sourish1 <sourishkumarmusib@gmail.com>
2018-08-17 12:49:58 +0530

    update userlist event

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,11 +21,31 @@ function scrollToBottom(){
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  //console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function(err){
+      if(err){
+          alert(err);
+          window.location.href = '/';
+      }else{
+          console.log('no error');
+      }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function(users) {
+    //console.log('Users List', users);
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
